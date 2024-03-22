9b189f3d59f8bf8ef90945b936c9257da7ee2e6d
Nathan Dennler <dennler@usc.edu>
2024-03-21 21:19:15 -0700

    adds tutorial readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PyLips'
+copyright = '2024, Interaction Lab'
+author = 'Nathaniel Dennler'
 
 release = '0.1'
 version = '0.1.0'
