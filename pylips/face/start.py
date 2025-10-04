7481a064d9d90a9bb89ebbc93f8393338e1a7238
Nathan Dennler <dennler@usc.edu>
2025-10-03 23:00:51 -0400

    updates play through browser

--- pylips/face/start.py
+++ pylips/face/start.py
@@ -6,6 +6,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--host', default='0.0.0.0', help='host IP address')
 parser.add_argument('--port', default=8000, type=int, help='port over which serve')
+parser.add_argument('--skip-audio-unlock', action='store_true',
+                    help='Disable the audio unlock overlay (auto-unlock audio).')
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*") #allow communication on the same IP (main use case for this package)
@@ -15,7 +17,9 @@ app.static_folder = 'static'
 @app.route('/face')
 @app.route('/face/<face_name>')
 def index(face_name='default'):
-    return render_template('index.html', face_name=face_name)
+    # Pass a flag to template to control overlay rendering
+    disable_overlay = getattr(app, 'disable_audio_overlay', False)
+    return render_template('index.html', face_name=face_name, disable_audio_overlay=disable_overlay)
 
 @app.route('/editor')
 def editor():
@@ -44,5 +48,7 @@ def handle_play_audio(data):
 
 if __name__ == '__main__':
     args = parser.parse_args()
+    # Store chosen overlay behavior on app config
+    app.disable_audio_overlay = args.skip_audio_unlock
     socketio.run(app, host=args.host, port=args.port)
     
